d9e01e2544bbe8415a76b1592df8192e108f0eb1
Filip <Filip@10.34.187.145>
2021-07-14 13:23:15 +0100

    Laying down the foundation and adding libraries

---
+++ src/main/org/Mp3_Convertor.java
@@ -0,0 +1,14 @@
+package main.org;
+
+public class Mp3_Convertor {
+
+	public Mp3_Convertor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello worlds");
+	}
+
+}
